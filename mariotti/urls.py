75c7d46e8a8d261cd4b6a2cfcbf33da7239c090c
alifanov <lifanov.a.v@gmail.com>
2014-11-22 15:17:42 +0300

    fix default CSS style

--- mariotti/urls.py
+++ mariotti/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='empty.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^service/$', TemplateView.as_view(template_name='service.html'), name='service'),
+    url(r'^news-all/$', TemplateView.as_view(template_name='news_all.html'), name='news-all'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
