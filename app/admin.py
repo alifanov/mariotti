f3e4f43e39e590c328008ff19c65895b955712da
alifanov <lifanov.a.v@gmail.com>
2014-11-25 16:55:25 +0300

    fix default CSS style

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from app.models import *
 # Register your models here.
+
+class NewsItemImageInline(admin.TabularInline):
+    model = NewsImage
+
+class NewsItemAdmin(admin.ModelAdmin):
+    inlines = [NewsItemImageInline,]
+
 admin.site.register(PortfolioItem)
 admin.site.register(ServiceItem)
-admin.site.register(NewsItem)
+admin.site.register(NewsItem, NewsItemAdmin)
