93811b6baaf1af903286a764b2d78d33f1286765
alifanov <lifanov.a.v@gmail.com>
2014-11-25 17:22:59 +0300

    fix default CSS style

--- app/admin.py
+++ app/admin.py
@@ -8,6 +8,13 @@ class NewsItemImageInline(admin.TabularInline):
 class NewsItemAdmin(admin.ModelAdmin):
     inlines = [NewsItemImageInline,]
 
+
+class ServiceItemImageInline(admin.TabularInline):
+    model = ServiceItemImage
+
+class ServiceItemAdmin(admin.ModelAdmin):
+    inlines = [ServiceItemImageInline,]
+
 admin.site.register(PortfolioItem)
-admin.site.register(ServiceItem)
+admin.site.register(ServiceItem, ServiceItemAdmin)
 admin.site.register(NewsItem, NewsItemAdmin)
